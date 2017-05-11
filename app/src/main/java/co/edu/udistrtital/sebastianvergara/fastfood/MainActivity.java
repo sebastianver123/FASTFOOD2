4fb6eafbb5517ffe626627ed8439075f47770454
sebastianver123 <sebastianver1234@gmail.com>
2017-05-10 20:27:23 -0500

    Activar el mapa de google

--- app/src/main/java/co/edu/udistrtital/sebastianvergara/fastfood/MainActivity.java
+++ app/src/main/java/co/edu/udistrtital/sebastianvergara/fastfood/MainActivity.java
@@ -1,13 +1,36 @@
 package co.edu.udistrtital.sebastianvergara.fastfood;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnsingin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnsingin = (Button)findViewById(R.id.btnin);
+
+         btnsingin.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+
+                 Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                 startActivity(intent);
+
+             }
+         });
     }
+
+
+
+
+
 }
