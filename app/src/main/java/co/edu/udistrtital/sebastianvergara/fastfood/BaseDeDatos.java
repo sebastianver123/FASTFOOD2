3ad829748fdcc714c74ef0874a609ac30d044605
sebastianver123 <sebastianver1234@gmail.com>
2017-05-17 18:28:35 -0500

    Almacenamiento de Datos en en SQLite

--- app/src/main/java/co/edu/udistrtital/sebastianvergara/fastfood/BaseDeDatos.java
+++ app/src/main/java/co/edu/udistrtital/sebastianvergara/fastfood/BaseDeDatos.java
@@ -3,6 +3,7 @@ package co.edu.udistrtital.sebastianvergara.fastfood;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
 /**
  * Created by Sebastián on 14/05/2017.
@@ -10,17 +11,44 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class BaseDeDatos extends SQLiteOpenHelper {
 
-    public BaseDeDatos(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public static final int VERSION_BASEDATOS = 1;
+    public static final String NOMBRE_BASEDATOS= "Datos.db";
+
+    public BaseDeDatos(Context context) {
+        super(context, NOMBRE_BASEDATOS, null, VERSION_BASEDATOS);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        db.execSQL(DatosDeTabla.CREAR_TABLA);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL(DatosDeTabla.SQL_DELETE_ENTRIES);
+        onCreate(db);
+
+    }
+
+    public static class DatosDeTabla implements BaseColumns {
+        public static final String NOMBRE_TABLA = "datosIngreso";
+        public static final String COLUMNA_ID = "id";
+        public static final String COLUMNA_CORREO = "correo";
+        public static final String COLUMNA_CONTRASEÑA = "contraseña";
+
+        private static final String TEXT_TYPE = " TEXT";
+        private static final String COMMA_SEP = ",";
+        private static final String CREAR_TABLA=
+                "CREATE TABLE " + DatosDeTabla.NOMBRE_TABLA + " (" +
+                        DatosDeTabla.COLUMNA_ID+ " INTEGER PRIMARY KEY," +
+                        DatosDeTabla.COLUMNA_CORREO+ TEXT_TYPE + COMMA_SEP +
+                        DatosDeTabla.COLUMNA_CONTRASEÑA + TEXT_TYPE + " )";
+
+        private static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + DatosDeTabla.NOMBRE_TABLA;
 
     }
 }
--- app/src/main/java/co/edu/udistrtital/sebastianvergara/fastfood/MainActivity.java
+++ app/src/main/java/co/edu/udistrtital/sebastianvergara/fastfood/MainActivity.java
@@ -1,14 +1,19 @@
 package co.edu.udistrtital.sebastianvergara.fastfood;
 
-import android.content.Intent;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnsingin;
+    EditText edCorreo;
+    EditText edContraseña;
 
 
     @Override
@@ -17,13 +22,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnsingin = (Button)findViewById(R.id.btnin);
+        edCorreo = (EditText)findViewById(R.id.correo);
+        edContraseña = (EditText)findViewById(R.id.pass);
+
+
+        final BaseDeDatos ayudaDatos = new BaseDeDatos(getApplicationContext());
 
          btnsingin.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
 
-                 Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-                 startActivity(intent);
+                 SQLiteDatabase db = ayudaDatos.getReadableDatabase();
+                 ContentValues misDatos = new ContentValues();
+                 misDatos.put(BaseDeDatos.DatosDeTabla.COLUMNA_CORREO,edCorreo.getText().toString());
+                 misDatos.put(BaseDeDatos.DatosDeTabla.COLUMNA_CONTRASEÑA,edContraseña.getText().toString());
+
+                 Long DatoGuardado = db.insert(BaseDeDatos.DatosDeTabla.NOMBRE_TABLA, BaseDeDatos.DatosDeTabla.COLUMNA_CORREO, misDatos);
+                 Toast.makeText(getApplicationContext(), "Ha creado su cuenta.", Toast.LENGTH_LONG).show();
 
              }
          });
