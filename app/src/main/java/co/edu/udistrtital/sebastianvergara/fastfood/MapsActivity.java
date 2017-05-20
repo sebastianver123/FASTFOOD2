f54ac0fd3c1cdab6072df4e8d4338ac1b7a17c5b
sebastianver123 <sebastianver1234@gmail.com>
2017-05-20 11:55:18 -0500

    Punto en Bogota

--- app/src/main/java/co/edu/udistrtital/sebastianvergara/fastfood/MapsActivity.java
+++ app/src/main/java/co/edu/udistrtital/sebastianvergara/fastfood/MapsActivity.java
@@ -22,6 +22,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
     }
 
 
@@ -38,10 +40,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+
+
+
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));}
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    }
+
 
 
 }
--- app/src/main/java/co/edu/udistrtital/sebastianvergara/fastfood/PantallaPrincipal.java
+++ app/src/main/java/co/edu/udistrtital/sebastianvergara/fastfood/PantallaPrincipal.java
@@ -3,9 +3,12 @@ package co.edu.udistrtital.sebastianvergara.fastfood;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class PantallaPrincipal extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -16,7 +19,6 @@ public class PantallaPrincipal extends AppCompatActivity implements OnMapReadyCa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_principal);
 
-
         SupportMapFragment mapFragment = (SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.MapView);
         mapFragment.getMapAsync(this);
     }
@@ -24,5 +26,13 @@ public class PantallaPrincipal extends AppCompatActivity implements OnMapReadyCa
     @Override
     public void onMapReady(GoogleMap googleMap) {
         miMapa = googleMap;
+
+        LatLng Bogota = new LatLng(4.6097100, -74.0817500);
+        miMapa.addMarker(new MarkerOptions().position(Bogota).title("Bogota"));
+        miMapa.moveCamera(CameraUpdateFactory.newLatLng(Bogota));
+
+
     }
+
+
 }
