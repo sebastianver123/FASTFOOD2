7cceb635c98f89c380f0b88a195aa34aaa970bf0
sebastianver123 <sebastianver1234@gmail.com>
2017-05-20 14:24:58 -0500

    Punto en Bogota

--- app/src/main/java/co/edu/udistrtital/sebastianvergara/fastfood/MapsActivity.java
+++ app/src/main/java/co/edu/udistrtital/sebastianvergara/fastfood/MapsActivity.java
@@ -51,6 +51,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
 
+
 }
 
 
--- app/src/main/java/co/edu/udistrtital/sebastianvergara/fastfood/PantallaPrincipal.java
+++ app/src/main/java/co/edu/udistrtital/sebastianvergara/fastfood/PantallaPrincipal.java
@@ -2,17 +2,23 @@ package co.edu.udistrtital.sebastianvergara.fastfood;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class PantallaPrincipal extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap miMapa;
+    EditText edBuscar;
+    Button btnBuscar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +27,29 @@ public class PantallaPrincipal extends AppCompatActivity implements OnMapReadyCa
 
         SupportMapFragment mapFragment = (SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.MapView);
         mapFragment.getMapAsync(this);
+
+        edBuscar = (EditText)findViewById(R.id.edBuscar);
+        btnBuscar = (Button)findViewById(R.id.btnBuscar);
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         miMapa = googleMap;
+        miMapa.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 
-        LatLng Bogota = new LatLng(4.6097100, -74.0817500);
-        miMapa.addMarker(new MarkerOptions().position(Bogota).title("Bogota"));
-        miMapa.moveCamera(CameraUpdateFactory.newLatLng(Bogota));
-
+        LatLng hamburguesa = new LatLng(4.628694, -74.065420);
+        Marker marker = miMapa.addMarker(new MarkerOptions().position(hamburguesa).title("El Corral"));
+        miMapa.moveCamera(CameraUpdateFactory.newLatLngZoom(hamburguesa,19));
 
     }
 
